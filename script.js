65497ba3f7190fa51c6e0b9f08b332405fc1863e
Prashant Salve <128687203+prashant04071995@users.noreply.github.com>
2024-04-05 18:57:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstWord(s) {
   // your code here
+if (str.trim() === '' || !str.includes(' ')) {
+    return str;}
+
+	
+} return str.split(' ')[0]; 
+
 }
 
 // Do not change the code below
